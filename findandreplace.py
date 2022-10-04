4e56eabfd10ae10fe9794b4a8b79537e74ce9552
sarahsameul <113873590+sarahsameul@users.noreply.github.com>
2022-10-04 22:41:13 +0530

    Create findandreplace.py

---
+++ findandreplace.py
@@ -0,0 +1,17 @@
+inputstring = ""
+chartofindandreplace = ""
+chartoreplacewith = ""
+replacedstring = ""
+
+inputstring = input("Enter a string: ")
+chartofindandreplace = input("Enter character to find and replace: ")
+chartoreplacewith = input("Enter character to replace with: ")
+
+for eachchar in list(inputstring):
+    if eachchar == chartofindandreplace:
+        replacedstring = replacedstring + chartoreplacewith
+    else:
+        replacedstring = replacedstring + eachchar
+
+print("Input String is: ", inputstring)
+print("Replaced String is: ", replacedstring)
