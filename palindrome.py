7ef048a1ec339fd032fb2ae242f9355e89380839
sarahsameul <113873590+sarahsameul@users.noreply.github.com>
2022-10-04 11:04:29 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+def reverse_str(str1):
+    if len(str1)==1:
+        return str1
+    else:
+        return reverse_str(str1[1:]) + str1[0]
+str1 = input("enter word or number:")
+str2 = reverse_str(str1)
+print(str2)
+if str1 == str2:
+    print(str1, "is a palindrome")
+else:
+    print(str1, "is not a palindrome")
