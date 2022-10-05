9f546ab9ffd3f0423499641e3c061b95a3b0809f
sarahsameul <113873590+sarahsameul@users.noreply.github.com>
2022-10-05 14:48:48 +0530

    Create operator.py

---
+++ operator.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter First Number: "))
+op = input("Enter Operator: ")
+num2 = int(input("Enter Second Number: "))
+if op == "+":
+    print(num1+num2)
+elif op == "-":
+    print(num1-num2)
+elif op == "*":
+    print(num1*num2)
+elif op == "/":
+    print(num1/num2)
+else:
+    print("Invalid Operator") 
