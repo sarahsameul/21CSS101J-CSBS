7bbe4da902be15ef0304aed7bb50fcd30f1ad9ad
sarahsameul <113873590+sarahsameul@users.noreply.github.com>
2022-10-06 22:38:33 +0530

    Create find_if_duplicates.py

---
+++ find_if_duplicates.py
@@ -0,0 +1,28 @@
+count = int(input("Enter total numbers to be input: "))
+
+numlist = []
+
+if count <= 0:
+    print("Count should be positive")
+else:
+    for i in range(count):
+        num = int(input("Enter Number: "))
+        numlist.append(num)
+        
+    #find if duplicates present
+    currentitem = 0
+    dupflag = 'N'
+    for eachnum in numlist: #for each number in list        
+        for i in range(count): #go through remaining items in list
+            if i != currentitem: #skip processing current item
+                if eachnum == numlist[i]: #compare our number with current item in list                    
+                    dupflag = 'Y'
+                    break
+        currentitem = currentitem + 1
+
+    if dupflag == 'Y':
+        print('Duplicates Present')
+    else:
+        print('No Duplicates')
+    
+                
