28f85da19085a09d804e4bb7b5b4b7e04a73ee7a
sarahsameul <113873590+sarahsameul@users.noreply.github.com>
2022-10-06 22:31:31 +0530

    Create choice_selection.py

---
+++ choice_selection.py
@@ -0,0 +1,10 @@
+choice = str(input("Enter a Choice Between A and B: "))
+
+if choice == 'A':
+    print("You've selected choice as A")
+elif choice == 'B':
+    print("You've selected choice as B")
+else:
+    print("You've neither selected A nor B")
+
+
