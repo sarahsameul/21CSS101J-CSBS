07486961605ac525a31d14c92651661604379702
sarahsameul <113873590+sarahsameul@users.noreply.github.com>
2022-10-06 23:00:51 +0530

    Create print_frequency_in_range.py

---
+++ print_frequency_in_range.py
@@ -0,0 +1,7 @@
+startnum = int(input("Enter First Number in Sequence: "))
+endnum = int(input("Enter Last Number in Sequence: "))
+frequency = int(input("Enter Frequency Range: "))
+
+for i in range(startnum, endnum+1):
+    if i%frequency == 0:
+        print(i)
