15b98ca9f339d8b665594ed02b10981c1c98f56a
sarahsameul <113873590+sarahsameul@users.noreply.github.com>
2022-10-06 22:51:45 +0530

    Create month_transform.py

---
+++ month_transform.py
@@ -0,0 +1,14 @@
+inputmonth = int(input("Enter a month in number between 1 and 12: "))
+
+months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+monthfound = 'N'
+i = 1
+for eachmon in months:
+    if i == inputmonth:
+        print("Month is:",eachmon)
+        monthfound = 'Y'        
+        break
+    i = i + 1
+
+if monthfound =='N':
+    print("Invalid Month Number")
