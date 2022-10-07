0c177abe61b26eee6e8de7e7c496d765dde2c658
sarahsameul <113873590+sarahsameul@users.noreply.github.com>
2022-10-07 23:08:32 +0530

    Create USDorINR.py

---
+++ USDorINR.py
@@ -0,0 +1,9 @@
+a = input("Would You Like To Convert To INR or USD? ")
+if a == "INR":
+    INR = int(input("Enter Amount In USD: "))
+    USD = round(INR*82)
+    print("Amount In INR Is",USD)
+if a == "USD":
+    inr = int(input("Enter Amount In INR: "))
+    usd = round(inr/82)
+    print("Amount In USD Is",usd)
