cbc631f264f52719337a170929e4d0952be77721
sarahsameul <113873590+sarahsameul@users.noreply.github.com>
2022-10-10 21:23:59 +0530

    Create sentence.py

---
+++ sentence.py
@@ -0,0 +1,13 @@
+text = input("Enter Sentence to Analyze: ")
+
+charcount = 0
+wordcount = 0
+for eachletter in list(text):
+    if eachletter == " ": 
+        wordcount = wordcount + 1
+        charcount = charcount - 1
+
+    charcount = charcount + 1
+    
+print("Word Count:", wordcount+1)
+print("Character Count without spaces:", charcount
