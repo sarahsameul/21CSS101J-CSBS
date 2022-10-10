d660f86866b16f54b1fce2eda34b84709f74fd6c
sarahsameul <113873590+sarahsameul@users.noreply.github.com>
2022-10-10 21:15:04 +0530

    Create date of birth.py

---
+++ date of birth.py
@@ -0,0 +1,5 @@
+dd = input("Enter The Date You Were Born in dd Format: ")
+mmm = input("Enter The Month You Were Born in mmm Format: ")
+yyyy = input("Enter The Year You Were Born in yyyy Format: ")
+
+print("Your Date Of Birth is",dd,"-",mmm.upper(),"-",yyyy
