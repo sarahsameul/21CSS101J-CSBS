fbe7cb063feb538fdf0b66a080f381130fb0087e
sarahsameul <113873590+sarahsameul@users.noreply.github.com>
2022-10-14 16:01:10 +0530

    Update date of birth.py

--- date of birth.py
+++ date of birth.py
@@ -1,3 +1,5 @@
+print("RA2211042010034")
+
 dd = input("Enter The Date You Were Born in dd Format: ")
 mmm = input("Enter The Month You Were Born in mmm Format: ")
 yyyy = input("Enter The Year You Were Born in yyyy Format: ")
