6cc896ee525bf2f6cd115d384d0460c6d07c5c91
sarahsameul <113873590+sarahsameul@users.noreply.github.com>
2022-10-14 16:46:42 +0530

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,8 +1,10 @@
+print("RA2211042010034")
+
 def reverse_str(str1):
     if len(str1)==1:
         return str1
     else:
         return reverse_str(str1[1:]) + str1[0]
-str1 = input("enter word or number:")
+str1 = input("Enter word or number:")
 str2 = reverse_str(str1)
 print(str2)
