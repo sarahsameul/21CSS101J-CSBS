c8c41a1297d116beb216976bfaa90c83bc19f87f
sarahsameul <113873590+sarahsameul@users.noreply.github.com>
2022-10-14 16:03:00 +0530

    Update division2.py

--- division2.py
+++ division2.py
@@ -1,3 +1,5 @@
- number1 =int(input ( "enter 1st number" ))
-number2= int(input ( "enter 2nd number" ))
+print("RA2211042010034")
+
+number1 =int(input ( "Enter 1st number" ))
+number2= int(input ( "Enter 2nd number" ))
 print("ratio is" , number1/number2 )
